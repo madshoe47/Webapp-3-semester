c25d79a7ba94b398c9aa72a8f4f902914f3b63a7
Jonathan-SS <56107465+Jonathan-SS@users.noreply.github.com>
2020-09-09 12:52:11 +0200

    prøver lige ting

--- javascript/spa-service.js
+++ javascript/spa-service.js
@@ -1,48 +1,32 @@
-export default class SpaService {
-  constructor(defaultPage) {
-    this.defaultPage = defaultPage;
-    this.pages = document.querySelectorAll(".page");
-    this.navItems = document.querySelectorAll(".tabbar a");
-    this.pageChange();
-  }
+"use strict";
 
-  // hide all pages
-  hideAllPages() {
-    for (let page of this.pages) {
-      page.style.display = "none";
-    }
-  }
-
-  // show page or tab
-  showPage(pageId) {
-    this.hideAllPages();
-    document.querySelector(`#${pageId}`).style.display = "block";
-    this.setActiveTab(pageId);
-  }
-
-  // sets active tabbar/ menu item
-  setActiveTab(pageId) {
-    for (let navItem of this.navItems) {
-      if (`#${pageId}` === navItem.getAttribute("href")) {
-        navItem.classList.add("active");
-      } else {
-        navItem.classList.remove("active");
-      }
-    }
-  }
-
-  // navigate to a new view/page by changing href
-  navigateTo(pageId) {
-    window.location.href = `#${pageId}`;
-  }
-
-  // set default page or given page by the hash url
-  // function is called 'onhashchange'
-  pageChange() {
-    let page = this.defaultPage;
-    if (window.location.hash) {
-      page = window.location.hash.slice(1);
-    }
-    this.showPage(page);
+// hide all pages
+function hideAllPages() {
+  let pages = document.querySelectorAll(".page");
+  for (let page of pages) {
+    page.style.display = "none";
   }
 }
+
+// show page or tab
+function showPage(pageId) {
+  hideAllPages();
+  document.querySelector(`#${pageId}`).style.display = "block";
+}
+
+// navigate to a new view/page by changing href
+function navigateTo(pageId) {
+  location.href = `#${pageId}`;
+}
+
+// set default page or given page by the hash url
+// function is called 'onhashchange'
+function pageChange() {
+  let page = "index";
+  if (location.hash) {
+    page = location.hash.slice(1);
+  }
+  showPage(page);
+}
+
+pageChange(); // called by default when the app is loaded for the first time
