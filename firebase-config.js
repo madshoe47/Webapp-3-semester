85fbb1eeab0c9e84c4c8292ac8f1e2b6a18b77db
madshoe47 <56107464+madshoe47@users.noreply.github.com>
2020-09-15 12:09:22 +0200

    Firebase config

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyD-_xFDbjKXb7TexhzpY8kmJzF7xfE1ns0",
+    authDomain: "webapp-bc1e9.firebaseapp.com",
+    databaseURL: "https://webapp-bc1e9.firebaseio.com",
+    projectId: "webapp-bc1e9",
+    storageBucket: "webapp-bc1e9.appspot.com",
+    messagingSenderId: "1092438804066",
+    appId: "1:1092438804066:web:f4c93c5d8513e7864bb33b",
+    measurementId: "G-45PN6BSGE9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
