5b60bd3f5d51051698740f7b285829fae17e55ee
Jonathan-SS <56107465+Jonathan-SS@users.noreply.github.com>
2020-09-16 11:58:58 +0200

    append get

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,70 @@
 "use strict"
+
+const _db = firebase.firestore();
+const abs = _db.collection("Abs");
+const legs = _db.collection("Legs");
+const as = _db.collection("Arms & Shoulder");
+const back = _db.collection("Back");
+const booty = _db.collection("Booty");
+const cardio = _db.collection("Cardio");
+const chest = _db.collection("Chest");
+const yoga = _db.collection("Yoga");
+
+
+
+
+// ========== READ ==========
+// watch the database ref for changes
+function getPrograms(part) {
+    part.onSnapshot(function (snapshotData) {
+        let programmer = [];
+        snapshotData.forEach(function (doc) {
+            let program = doc.data();
+            program.id = doc.id;
+            programmer.push(program);
+        });
+        console.log(programmer)
+        appendPrograms(programmer, part)
+
+    });
+}
+
+
+
+console.log(current)
+
+getPrograms(current)
+
+let current = window.location.hash.replace(/#/g, '')
+
+function appendPrograms(programs, part) {
+    let HTMLtemplate = `"<h1 class="overskrift">${part}</h1>"`;
+    for (const program of programs) {
+        console.log(program)
+        HTMLtemplate += /*html*/`
+         
+        <article class="program">
+            <div class="img_t_l">
+                <img class="proIMG" src="${program.img}" alt="">
+                <p class="time"><img src="img/Icon ionic-md-stopwatch.svg" alt="Stopwatch icon">:${program.time}</p>
+                <p class="location"><img src="img/Icon material-location-on.svg" alt="Location icon">:${program.location}</p>
+            </div>
+            <div>
+                <h1 class="title">${program.title}</h1>
+                <div class="purpose">
+                    <p class="marginnul"> ${program.purpose}</p>
+                </div>
+
+
+                <p class="description">${program.description}</p>
+            </div>
+        </article>
+        `
+    }
+    document.querySelector("#abs").innerHTML = HTMLtemplate
+
+}
+
+
+
+
--- javascript/programmer.js
+++ javascript/programmer.js
@@ -1,18 +1,2 @@
 "use strict";
 
-
-
-
-
-`
-<article>
-        <img class="proIMG" src="${}" alt="">
-        <h1 class="title">${}</h1>
-        <div class="purpose">
-            <p></p>
-        </div>
-        <p class="description">${}</p>
-        <p class="time"><img src="img/Icon ionic-md-stopwatch.svg" alt="Stopwatch icon">:${}</p>
-        <p class="location"><img src="img/Icon material-location-on.svg" alt="Location icon">:{}</p>
-    </article>
-`
